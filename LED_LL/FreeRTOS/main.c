834ab6177d4ad7f9f8d7839d48b89e9da07c7105
gchh <skring.2008@163.com>
2018-10-12 14:51:48 +0800

    freeRTOS

---
+++ LED_LL/FreeRTOS/main.c
@@ -0,0 +1,150 @@
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_gpio.h"
+#if defined(USE_FULL_ASSERT)
+#include "stm32_assert.h"
+#endif /* USE_FULL_ASSERT */
+#include "led.h"
+#include "mco.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+void     SystemClock_Config(void);
+void led_Serve(void* pvParam);
+
+#define LED_STACK_SIZE  10
+TaskHandle_t LedTaskHandle = NULL;
+
+LL_RCC_ClocksTypeDef RCC_CLOCKS;
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+    /* Configure the system clock to 72 MHz */
+    SystemClock_Config();
+    
+    /* -2- Configure IO in output push-pull mode to drive external LED */
+    LED_GPIO_Config();
+
+    MCO_config(LL_RCC_MCO1SOURCE_HSE);
+    
+    LL_RCC_GetSystemClocksFreq(&RCC_CLOCKS);
+
+    xTaskCreate( led_Serve, 
+                 (char const*)"led", 
+                 LED_STACK_SIZE, 
+                 NULL,
+                 0, 
+                 &LedTaskHandle);  
+
+	//Æô¶¯µ÷¶ÈÆ÷
+	vTaskStartScheduler();  
+                 
+    while(1){}
+}
+
+
+void led_Serve(void* pvParam)
+{
+    while(1)
+    {
+        LED1Toggle();
+        vTaskDelay(100);
+    }
+}
+
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 72000000
+  *            HCLK(Hz)                       = 72000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLLMUL                         = 9
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+    /* Set FLASH latency */
+    LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
+
+    /* Enable HSE oscillator */
+    LL_RCC_HSE_Enable();
+    while(LL_RCC_HSE_IsReady() != 1)
+    {
+    };
+
+    /* Main PLL configuration and activation */
+    LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE_DIV_1, LL_RCC_PLL_MUL_9);
+
+    LL_RCC_PLL_Enable();
+    while(LL_RCC_PLL_IsReady() != 1)
+    {
+    };
+
+    /* Sysclk activation on the main PLL */
+    LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+    LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+    while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+    {
+    };
+
+    /* Set APB1 & APB2 prescaler*/
+    LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+    LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+    /* Set systick to 1ms in using frequency set to 72MHz */
+    LL_Init1msTick(72000000);
+
+    /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
+    LL_SetSystemCoreClock(72000000);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
