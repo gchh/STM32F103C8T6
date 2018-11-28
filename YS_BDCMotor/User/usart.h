da15e6c98adc9e63b6e898e6ddccb38ddde7ea41
gchh <skring.2008@163.com>
2018-11-29 00:01:16 +0800

    xiugai

--- YS_BDCMotor/User/usart.h
+++ YS_BDCMotor/User/usart.h
@@ -5,21 +5,18 @@
 #include <stdio.h>
 
 //115200-8-N-1
-#define USARTx             	 USART1
-#define USARTx_BaudRate	     115200
+#define USART             	 USART1
+#define BaudRate	         115200
 #define USART_RCC         	 RCC_APB2Periph_USART1
 
-#define USARTx_GPIO_RCC       RCC_APB2Periph_GPIOA
-#define USARTx_GPIO_PORT      GPIOA  
-#define USARTx_TX_PIN         GPIO_Pin_9  // out
-#define USARTx_RX_PIN         GPIO_Pin_10 // in  
-
-#define USARTx_IRQHANDLER     USART1_IRQHandler
-#define USARTx_IPQn           USART1_IRQn  
+#define USART_GPIO_RCC       RCC_APB2Periph_GPIOA
+#define USART_GPIO_PORT      GPIOA  
+#define USART_TX_PIN         GPIO_Pin_9  // out
+#define USART_RX_PIN         GPIO_Pin_10 // in  
 
 
-void USARTx_GPIO_Init(void);
-void USARTx_Config(void);
+void USART_GPIO_Init(void);
+void USART_Config(void);
 
 
 #endif
